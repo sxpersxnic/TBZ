13f374442452cff02f7ee21ebb2cc9a0b7f784aa
sxpersxnic <102521739+sxpersxnic@users.noreply.github.com>
2024-11-28 13:49:17 +0100

    Update Main.java

--- m320-OOP/D-Delegation/D2/sxpersxnic/src/Main.java
+++ m320-OOP/D-Delegation/D2/sxpersxnic/src/Main.java
@@ -1,11 +1,19 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("------------D2------------");
-
     Flight flight = Seed.seedFlight();
-
-    flight.printPassengers();
+    List<Passenger> passengers = flight.getPassengers();
     
+    // Enter firstName ...
+    String firstName = "firstName";
+    // Enter lastName ...
+    String lastName = "lastName";
+    String name = firstName + " " + lastName;
+    Passenger you = new Passenger(name);
+
+    passengers.add(you);
+    flight.setPassengers(passengers);
+    flight.printPassengers();
     System.out.println("--------------------------");
   }
 }
