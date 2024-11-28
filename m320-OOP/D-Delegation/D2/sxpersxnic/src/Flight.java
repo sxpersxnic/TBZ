18ee54566ad569d9cf6a017d7246d2b98afe85a6
sxpersxnic <102521739+sxpersxnic@users.noreply.github.com>
2024-11-28 13:45:22 +0100

    Update Flight.java

--- m320-OOP/D-Delegation/D2/sxpersxnic/src/Flight.java
+++ m320-OOP/D-Delegation/D2/sxpersxnic/src/Flight.java
@@ -1,9 +1,26 @@
 public class Flight {
-  List<Passenger> passengers = new ArrayList<Passenger>();
 
+  // <---------Passengers--------->
+  private List<Passenger> passengers = new ArrayList<Passenger>();
+
+  // <---------Constructor--------->
+  public Flight(List<Passenger> passengers) { this.passengers = passengers }
+  
+  // <---------printPassengers--------->
   public void printPassengers() {
     for (Passenger passenger : passengers) {
       passenger.printName();
     }
   }
+
+  // <---------Getter--------->
+  public List<Passenger> getPassengers() {
+    return passengers;
+  } 
+  
+  // <---------Setter--------->
+  public void setPassengers(List<Passenger> passengers) {
+    this.passengers = passengers;
+  }
+  
 }
