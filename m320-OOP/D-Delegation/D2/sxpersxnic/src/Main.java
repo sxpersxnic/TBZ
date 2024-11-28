244e1e312323305f06216cda72adde65783f350f
sxpersxnic <102521739+sxpersxnic@users.noreply.github.com>
2024-11-28 14:04:31 +0100

    Update Main.java

--- m320-OOP/D-Delegation/D2/sxpersxnic/src/Main.java
+++ m320-OOP/D-Delegation/D2/sxpersxnic/src/Main.java
@@ -1,19 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("------------D2------------");
+
+    Scanner scanner = new Scanner(System.in);
     Flight flight = Seed.seedFlight();
     List<Passenger> passengers = flight.getPassengers();
     
-    // Enter firstName ...
-    String firstName = "firstName";
-    // Enter lastName ...
-    String lastName = "lastName";
+    System.out.println("------------D2------------");
+    
+    System.out.print("Enter your firstname: ");
+    String firstName = scanner.nextLine();
+    
+    System.out.print("Enter your lastname: ");
+    String lastName = scanner.nextLine();
+    
     String name = firstName + " " + lastName;
     Passenger you = new Passenger(name);
 
     passengers.add(you);
     flight.setPassengers(passengers);
     flight.printPassengers();
+  
     System.out.println("--------------------------");
   }
 }
