39f68deafc8091ab10849f732b31c8af2ab2fe2c
sxpersxnic <102521739+sxpersxnic@users.noreply.github.com>
2024-11-28 14:37:22 +0100

    Update Flight.java

--- m320-OOP/D-Delegation/D2/sxpersxnic/src/Flight.java
+++ m320-OOP/D-Delegation/D2/sxpersxnic/src/Flight.java
@@ -2,40 +2,18 @@ import java.util.Random;
 
 public class Flight {
 
-  // <---------Passengers--------->
-  private List<Passenger> passengers = new ArrayList<Passenger>();
+    // <---------Passengers--------->
+    private List<Passenger> passengers = new ArrayList<Passenger>();
 
-  // <---------Constructor--------->
-  public Flight(List<Passenger> passengers) { this.passengers = passengers }
-  
-  // <---------printPassengers--------->
-  public void printPassengers() {
-    System.out.println("Passengers of Flight " + randomFlightNumber() + ":");
-    System.out.println("Firstname Lastname");
-    for (Passenger passenger : passengers) {
-      passenger.printName();
+    // <---------Constructor--------->
+    public Flight(List<Passenger> passengers) { this.passengers = passengers }
+
+    // <---------printPassengers--------->
+    public void printPassengers() {
+        System.out.println("Passengers of Flight " + Util.randomFlightNumber() + ":");
+        System.out.println("Firstname Lastname");
+        for (Passenger passenger : passengers) {
+            passenger.printName();
+        }
     }
-  }
-
-  public String randomFlightNumber() {
-    
-    Random random = new Random();
-    String[] chars = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"; "U", "V", "W", "X", "Y", "Z"};
-    
-    String firstChar = chars[random.nextInt(chars.length)];
-    String secondChar = chars[random.nextInt(chars.length];
-
-    return firstChar + secondChar + "-" + random.nextInt(10) + random.nextInt(10) + random.nextInt(10);
-  }
-
-  // <---------Getter--------->
-  public List<Passenger> getPassengers() {
-    return passengers;
-  } 
-  
-  // <---------Setter--------->
-  public void setPassengers(List<Passenger> passengers) {
-    this.passengers = passengers;
-  }
-  
 }
