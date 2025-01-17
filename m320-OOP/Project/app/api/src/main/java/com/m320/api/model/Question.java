19438371803b0bd4a1d27236e200d9f1b5fff915
t.jahari <tristan.jahari@edu.tbz.ch>
2025-01-17 13:16:59 +0100

    Updated Questions

---
+++ m320-OOP/Project/app/api/src/main/java/com/m320/api/model/Question.java
@@ -0,0 +1,26 @@
+package com.m320.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private UUID userId;
+
+    private String question;
+
+    private String description;
+}
---
+++ m320-OOP/Project/app/api/src/main/java/com/m320/api/payload/mapper/QuestionMapper.java
@@ -0,0 +1,15 @@
+package com.m320.api.payload.mapper;
+
+import com.m320.api.model.Question;
+import com.m320.api.model.User;
+import com.m320.api.payload.dto.request.UserRequestDTO;
+
+public class QuestionMapper {
+
+    public static User fromDTO(UserRequestDTO dto)
+    {
+        Question question = new Question();
+
+        question.setId()
+    }
+}
