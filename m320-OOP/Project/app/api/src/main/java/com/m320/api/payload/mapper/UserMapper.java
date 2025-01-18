51c8ff96ce9b023281db4dfbf4e37cc40eb7ab43
sxpersxnic <melvin@kampus.ch>
2025-01-18 17:52:51 +0100

    Clean up

--- m320-OOP/Project/app/api/src/main/java/com/m320/api/payload/mapper/UserMapper.java
+++ m320-OOP/Project/app/api/src/main/java/com/m320/api/payload/mapper/UserMapper.java
@@ -1,18 +1,9 @@
 package com.m320.api.payload.mapper;
 
 import com.m320.api.model.User;
-import com.m320.api.payload.dto.request.UserRequestDTO;
 import com.m320.api.payload.dto.response.UserResponseDTO;
 
 public class UserMapper {
-
-    public static User fromDTO(UserRequestDTO dto) {
-        User user = new User();
-
-        user.setEmail(dto.getEmail());
-
-        return user;
-    }
 
     public static UserResponseDTO toDTO(User src) {
         UserResponseDTO dto = new UserResponseDTO();
