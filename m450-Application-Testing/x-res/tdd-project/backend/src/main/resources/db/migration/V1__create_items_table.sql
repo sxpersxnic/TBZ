393169ba689c01974aa8e033aa80fb2d56008cfb
sxpersxnic <102521739+sxpersxnic@users.noreply.github.com>
2026-01-16 16:46:01 +0100

    m450 tdd project: ci-pipeline - implemented ci pipeline

--- m450-Application-Testing/x-res/tdd-project/backend/src/main/resources/db/migration/V1__create_items_table.sql
+++ m450-Application-Testing/x-res/tdd-project/backend/src/main/resources/db/migration/V1__create_items_table.sql
@@ -1,8 +1,8 @@
 -- PostgreSQL migration script
 CREATE TABLE items (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    assigned_user_id UUID,
     title VARCHAR(50) NOT NULL UNIQUE,
     description VARCHAR(200),
     tags TEXT[] DEFAULT ARRAY[]::TEXT[]
-    assigned_user_id UUID
 );
