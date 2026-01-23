a5d19248bbd03232dad805547a4cdaee2b81f132
sxpersxnic <102521739+sxpersxnic@users.noreply.github.com>
2026-01-23 16:37:52 +0100

    Add assigned_user_id and change is_completed constraint

--- m450-Application-Testing/x-res/tdd-project/backend/src/main/resources/db/migration/V1__create_items_table.sql
+++ m450-Application-Testing/x-res/tdd-project/backend/src/main/resources/db/migration/V1__create_items_table.sql
@@ -1,9 +1,10 @@
 -- PostgreSQL migration script
 CREATE TABLE items (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    assigned_user_id UUID,
     title VARCHAR(50) NOT NULL UNIQUE,
     description VARCHAR(200),
-    is_completed BOOLEAN DEFAULT false,
+    is_completed BOOLEAN NOT NULL DEFAULT false,
     due_date TIMESTAMP DEFAULT NULL,
     priority VARCHAR(20) DEFAULT 'MEDIUM'
 );
