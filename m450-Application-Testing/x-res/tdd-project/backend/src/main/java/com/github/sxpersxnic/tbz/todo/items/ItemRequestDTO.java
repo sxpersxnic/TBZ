8ad6440fc540a17607bdf90408dfbf3c5e52385f
sxpersxnic <102521739+sxpersxnic@users.noreply.github.com>
2026-01-23 16:42:19 +0100

    Change validation annotation from NotBlank to NotNull

--- m450-Application-Testing/x-res/tdd-project/backend/src/main/java/com/github/sxpersxnic/tbz/todo/items/ItemRequestDTO.java
+++ m450-Application-Testing/x-res/tdd-project/backend/src/main/java/com/github/sxpersxnic/tbz/todo/items/ItemRequestDTO.java
@@ -1,6 +1,6 @@
 package com.github.sxpersxnic.tbz.todo.items;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
